1fceda0ce4fb5d51435a944637814e550126de11
cohenaj194 <cohenaj194@gmail.com>
2016-02-27 18:53:10 -0500

    pushing to github

--- gitSetup.sh
+++
@@ -1,8 +0,0 @@
-#sets up the git project, adds everything inside and commits it all
-#copy into a new project containing files, run and remove before pushing to github
-git config --global user.name "cohenaj194"
-git config --global usre.email "cohenaj194@gmail.com"
-git init .
-git add .
-git commit -m "initial commit"
-rm gitSetup.sh
